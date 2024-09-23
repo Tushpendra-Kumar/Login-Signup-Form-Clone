29ba33574e05ab837e3bb8a4dccc4157135162c1
tushu <ritiksingh7055072196>
2024-09-23 19:07:19 +0530

    Frist Commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const container = document.querySelector('.container');
+const LoginLink = document.querySelector('.SignInLink');
+const RegisterLink = document.querySelector('.SignUpLink');
+
+RegisterLink.addEventListener('click', () =>{
+    container.classList.add('active');
+})
+
+LoginLink.addEventListener('click', () => {
+    container.classList.remove('active');
+})
